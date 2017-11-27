47e8031e16eb0429e4be823e11c5bcd6fba7f764
Nei <neigdias@gmail.com>
2017-11-28 00:12:28 +0100

    initial commit - working

---
+++ src/main/java/br/ngd/batch/BatchProjectApplication.java
@@ -0,0 +1,12 @@
+package br.ngd.batch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BatchProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BatchProjectApplication.class, args);
+	}
+}
